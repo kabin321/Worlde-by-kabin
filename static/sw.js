2a25265c5c0f93e432cb8c4263c9274294e9a67d
kabin321 <kabin6650@gmail.com>
2026-03-10 17:02:00 +0545

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "wordle-v1";
+const STATIC_FILES = [
+  "/",
+  "/static/css/style.css",
+  "/static/js/app.js",
+  "/static/manifest.json",
+  "/static/icons/icon-192.png",
+  "/static/icons/icon-512.png",
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith("/api/")) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
